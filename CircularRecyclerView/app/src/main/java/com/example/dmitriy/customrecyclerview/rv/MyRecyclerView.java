c916da446c1fc29ef6444c36c769c2ababbf49a2
DimaKhaver\Dima <xaverdima@gmail.com>
2017-11-20 11:33:13 +0200

    Put new solution in master branch using custom LinkedList

--- CircularRecyclerView/app/src/main/java/com/example/dmitriy/customrecyclerview/rv/MyRecyclerView.java
+++ CircularRecyclerView/app/src/main/java/com/example/dmitriy/customrecyclerview/rv/MyRecyclerView.java
@@ -27,7 +27,7 @@ public class MyRecyclerView extends RecyclerView {
 
     @Override
     public boolean fling(int velocityX, int velocityY) {
-        velocityX *= 0.2;
+        velocityX *= 0.1;
       //velocityY *= 0.1;
         return super.fling(velocityX, velocityY);
     }
